c206dd8373e1c9d91314e6b1d2add5e8e7c88752
Abobora <lucasclucas7@gmail.com>
2022-12-04 00:13:52 -0300

    Curso Web-Moderno

--- Curso Web-Moderno/bootstrap/exercicios/js/navegacao.js
+++ Curso Web-Moderno/bootstrap/exercicios/js/navegacao.js
@@ -12,7 +12,9 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
-                eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1])
+                const resultado = (html.match(/\<script\>([\s\S]*)\<\/script\>/))
+                // resultado && eval(resultado[1])
+                if(resultado && resultado.length >= 2) eval(resultado[1])
             })
     }
 

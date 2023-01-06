72c4e99238752ad5043ee4aae075c73f2d5f869e
Abobora <lucasclucas7@gmail.com>
2023-01-05 21:57:56 -0300

    Vue

--- Curso Web-Moderno/vue/montyhall/src/components/Gift.vue
+++ Curso Web-Moderno/vue/montyhall/src/components/Gift.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="gift">
-    <div class="gift-tie-1"></div>
-    <div class="gift-tie-2"></div>
-    <div class="gift-top"></div>
-    <div class="gift-body"></div>
-  </div>
+    <div class="gift">
+        <div class="gift-tie-1"></div>
+        <div class="gift-tie-2"></div>
+        <div class="gift-top"></div>
+        <div class="gift-body"></div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "GiftComponent",
-};
+    name: 'Gift'
+}
 </script>
 
 <style>

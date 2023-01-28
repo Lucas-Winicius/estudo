1be7c6a5e1f502bd17e8f78c62e23d2f4aa46a21
Abobora <lucasclucas7@gmail.com>
2023-01-28 15:26:06 -0300

    Xalculadora de IMC finalizada

--- JavaScript e TypeScript - Basico ao avançado/Aula_26/index.js
+++ JavaScript e TypeScript - Basico ao avançado/Aula_26/index.js
@@ -1,7 +1,7 @@
 const inputs = document.querySelectorAll('input')
-
 const setarErro = (inputIndex, texto) => {
     const botao = document.querySelector('button')
+
     if(inputIndex == 0) {
         const erro = document.querySelector('#erroPeso')
         inputs[0].style.border = '2px solid red'
@@ -17,6 +17,20 @@ const setarErro = (inputIndex, texto) => {
         erro.innerHTML = texto
         botao.style.backgroundColor = '#cc3300'
     }
+}
+
+const setarIMC = (texto, cor, corTexto = 'black') => {
+    const displayIMC = document.querySelector("#exibirIMC")
+    const botao = document.querySelector('button')
+
+    document.body.style.backgroundColor = cor
+    botao.style.backgroundColor = cor
+    botao.style.color = corTexto
+    displayIMC.innerHTML = texto
+    displayIMC.style.backgroundColor = cor
+    displayIMC.style.color = corTexto
+    displayIMC.style.display = 'block'
+
 }
 
 const validarPeso = () => {
@@ -48,12 +62,47 @@ const validarAltura = () => {
         return;
     }
 
+    return inputAltura
+
 }
 
 const calcularIMC = e => {
     e.preventDefault();
     const peso = validarPeso()
     const altura = validarAltura()
+
+    if(!peso || !altura) return;
+
+    const imc = peso / (altura * altura)
+
+    console.log(imc)    
+
+    if(imc <= 16.9) { setarIMC('muito abaixo do peso', '#ff3300', 'white') }
+
+    else if (imc >= 17 && imc <=18.4) { setarIMC('Abaixo do peso', '#ffb700') }
+    
+    else if (imc >= 18.5 && imc <= 24.9) { setarIMC('Peso normal', '#005414', 'white') }
+    
+    else if (imc >= 25 && imc <= 29.9) { setarIMC('Acima do peso', '#e0fc05') }
+    
+    else if (imc >= 30 && imc <= 34.9) { setarErro('Obesidade grau 1', '#fc6805') }
+    
+    else if(imc >= 35 && imc <= 39.9) { setarIMC('Obesidade grau 2', '#fc3205', 'white') }
+    
+    else if (imc >= 40) { setarIMC('Obesidade grau 3', '#fc1505', 'white') }
+    
+    else { setarIMC('Erro ao calcular imc', 'red', 'white') }
+    
 }
 
 addEventListener('submit', e => calcularIMC(e))
+
+onchange = e => {
+    document.body.style.backgroundColor = 'rgb(0, 95, 68)'
+    document.querySelector('button').style.backgroundColor = 'rgb(0, 95, 68)'
+    document.querySelector('button').style.color = 'white'
+    document.querySelector('#exibirIMC').style.display = 'none'
+
+    inputs.forEach(element => element.style.border = '2px solid gray');
+    document.querySelectorAll('.erro').forEach(element => element.innerHTML = '')
+}

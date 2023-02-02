9062f60c440e533486dee7e79ce0cbf9f79a299e
Abobora <lucasclucas7@gmail.com>
2023-02-02 14:44:37 -0300

    Inicio do novo curso

---
+++ JavaScript e TypeScript - Basico ao avançado/JavaScript/Aula_03/index.js
@@ -0,0 +1,5 @@
+const nome = 'Lucas W.';
+
+const horario = () => `${new Date().getHours()}h${new Date().getMinutes()}`;
+
+console.log(`Meu nome é ${nome} E estou aprendendo JavaScript as ${horario()}`);

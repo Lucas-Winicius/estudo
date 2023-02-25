e22ed4d500b37b72f7f0c4012488fd30314d51c9
Abobora <lucasclucas7@gmail.com>
2023-02-24 23:25:42 -0300

    Métodos de instância e estáticos

---
+++ JavaScript e TypeScript - Basico ao avançado/JavaScript/Aula_84/aula84.js
@@ -0,0 +1,27 @@
+class ControleRemoto {
+  constructor(tv) {
+    this.tv = tv;
+    this.volume = 0;
+  }
+
+  aumentarVolume() {
+    this.volume += 2;
+  }
+
+  diminuirVolume() {
+    this.volume -= 2;
+  }
+
+  static trocarPilha() {
+    console.log('Trocando...');
+  }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+ControleRemoto.trocarPilha();
+Math.random()

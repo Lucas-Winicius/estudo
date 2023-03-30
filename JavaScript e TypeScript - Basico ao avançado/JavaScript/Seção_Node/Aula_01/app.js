2253cc03531a812a12e211e1c59a97eb761f376e
Lucas-Winicius <lucasclucas7@gmail.com>
2023-03-30 10:37:44 -0300

    Modulos ( 1 )

---
+++ JavaScript e TypeScript - Basico ao avançado/JavaScript/Seção_Node/Aula_01/app.js
@@ -0,0 +1,7 @@
+const mod1 = require('./mod1');
+// const falaNome = require('./mod1').falaNome
+// const falaNome = mod1.falaNome;
+const { nome, sobrenome, falaNome } = require('./mod1');
+
+console.log(falaNome());
+console.log(nome);

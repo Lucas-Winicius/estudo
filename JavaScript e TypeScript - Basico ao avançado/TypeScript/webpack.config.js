bad53aeaadf6758c4b04c4793b0011354d4bd81e
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-08 14:02:47 -0300

    webpack update

--- JavaScript e TypeScript - Basico ao avançado/TypeScript/webpack.config.js
+++ JavaScript e TypeScript - Basico ao avançado/TypeScript/webpack.config.js
@@ -8,8 +8,11 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        loader: "ts-loader",
         exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.frontend.json",
+        },
       },
     ],
   },
@@ -18,7 +21,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "frontend", "assets", "scripts"),
   },
   devtool: "source-map",
 };

121b563ca619ef764706d7a3a6610747ca403518
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-08 13:48:32 -0300

    Webpack config

---
+++ JavaScript e TypeScript - Basico ao avançado/TypeScript/source/A018-webpack/button.ts
@@ -0,0 +1,6 @@
+const body = document.querySelector("body");
+body.innerHTML += "<button>Botão</button>";
+
+console.log("Sou o arquivo do botão!");
+
+export default "";
---
+++ JavaScript e TypeScript - Basico ao avançado/TypeScript/source/A018-webpack/index.ts
@@ -0,0 +1,7 @@
+import "./button";
+console.log("Hello");
+
+const body = document.querySelector("body");
+body.innerHTML += "Hello, TypeScript!";
+
+export default "";
